7423c40ed15902da6d4c7272ff2e42ce626db23e
duongdinhtam <122358939+duongdinhtam@users.noreply.github.com>
2023-01-10 22:54:35 +0700

    up

---
+++ js/music-list-chvtx.js
@@ -0,0 +1,22 @@
+let allMusic = [
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+    
+  },
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+  },
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+  },
+
+];
---
+++ js/music-list-kvsl.js
@@ -0,0 +1,23 @@
+let allMusic = [
+  {
+    name: "Kỳ vọng sai lầm",
+    artist: "Nguyễn Đình Vũ",
+    img: "kyvongsailam",
+    src: "kyvongsailam"
+  },
+
+  {
+    name: "Kỳ vọng sai lầm",
+    artist: "Nguyễn Đình Vũ",
+    img: "kyvongsailam",
+    src: "kyvongsailam"
+  },
+  
+  {
+    name: "Kỳ vọng sai lầm",
+    artist: "Nguyễn Đình Vũ",
+    img: "kyvongsailam",
+    src: "kyvongsailam"
+  },
+
+];
