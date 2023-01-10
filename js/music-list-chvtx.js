89820f356957dcd3689f3c0cde27006e37ebab35
duongdinhtam <122358939+duongdinhtam@users.noreply.github.com>
2023-01-10 17:32:50 +0700

    up

---
+++ js/music-list-chvtx.js
@@ -0,0 +1,21 @@
+let allMusic = [
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+  },
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+  },
+  {
+    name: "Có hẹn với thanh xuân",
+    artist: "MONSTAR",
+    img: "cohenvoitx",
+    src: "cohenvoitx"
+  },
+
+];
---
+++ js/music-list-ymcdql.js
@@ -0,0 +1,24 @@
+let allMusic = [
+  {
+    name: "Yêu mấy cũng đừng quay lại",
+    artist: "Mr.Siro",
+    img: "22",
+    src: "yeumaycungdql"
+  },
+
+  {
+    name: "Yêu mấy cũng đừng quay lại",
+    artist: "Mr.Siro",
+    img: "22",
+    src: "yeumaycungdql"
+  },
+
+  
+  {
+    name: "Yêu mấy cũng đừng quay lại",
+    artist: "Mr.Siro",
+    img: "22",
+    src: "yeumaycungdql"
+  },
+
+];
